23e7a9093d121ba3a36e99b1325f9bf3c6b65847
displayworld7 <displayword7@gmail.com>
2026-04-02 15:34:31 +0300

    fix

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,6 +1,13 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
 
 const prisma = new PrismaClient({
+  adapter,
   log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
 });
 
